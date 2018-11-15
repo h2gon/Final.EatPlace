4338135a536e4daa6a1d7ae9398bd44a765c1abb
user2 <user2@KH_C>
2018-11-15 20:21:42 +0900

    service

---
+++ Final/src/kh/c/five/service/EatService.java
@@ -0,0 +1,5 @@
+package kh.c.five.service;
+
+public interface EatService {
+
+}
---
+++ Final/src/kh/c/five/serviceimpl/EatServiceImpl.java
@@ -0,0 +1,10 @@
+package kh.c.five.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import kh.c.five.service.EatService;
+
+@Service
+public class EatServiceImpl implements EatService {
+
+}
